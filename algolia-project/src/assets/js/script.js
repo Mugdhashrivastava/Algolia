15f9a1072a46a88bb464167faead02b02a405dd3
Mugdha Shrivastava <shrivastavamugdha19@gmail.com>
2024-08-13 11:33:47 +0530

    npm install --save-dev parcel

---
+++ algolia-project/src/assets/js/script.js
@@ -0,0 +1,22 @@
+import algoliasearch from 'algoliasearch/lite';
+import instantsearch from 'instantsearch.js';
+import { searchBox, hits } from 'instantsearch.js/es/widgets';
+
+searchClient = algoliasearch('YourApplicationID', 'YourSearchOnlyAPIKey');
+
+const search = instantsearch({
+  indexName: 'demo_ecommerce',
+  searchClient,
+});
+
+search.addWidgets([
+  searchBox({
+    container: "#searchbox"
+  }),
+
+  hits({
+    container: "#hits"
+  })
+]);
+
+search.start();
